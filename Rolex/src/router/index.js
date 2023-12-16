ca72ce825574118cbee8807a5c9fbd9d0d4f4cb5
RubenBervoets <ruben.bervoets@student.pxl.be>
2023-12-16 17:12:30 +0100

    Gewerkt aan de router

--- Rolex/src/router/index.js
+++ Rolex/src/router/index.js
@@ -1,23 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from './views/Home'
+// import Products from './views/Products'
+// import ProductDetail from './views/ProductDetail'
+// import Login from './views/Login'
+// import Winkelmandje from './views/Winkelmandje'
+import PageNotFoundView from '@/views/PageNotFoundView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            component: Home
+        },
+        {
+            path: '/Products',
+            component: Products
+        },
+        {
+            path: '/Products/:name',
+            name: 'ProductDetail',
+            component: ProductDetail
+        },
+        {
+            path: '/:notFound(.*)',
+            component: PageNotFoundView
+        }
+    ]
 })
 
 export default router
--- Rolex/src/views/Home.vue
+++ Rolex/src/views/Home.vue
@@ -1,5 +1,14 @@
-<script setup>
-</script>
-
 <template>
+    <header>
+        "Hello"
+    </header>
+    <div>
+        <RouterView>{{ PopularProducts }}</RouterView>
+    </div>
 </template>
+
+<script setup>
+import { RouterView } from 'vue-router';
+import PopularProducts from '../components/PopularProducts.vue';
+
+</script>
