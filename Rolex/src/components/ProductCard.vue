37629fa8686ef13f87730aa0b8d4d05d8b09301f
RubenBervoets <ruben.bervoets@student.pxl.be>
2023-12-20 17:14:38 +0100

    Probleem met het ophalen van de data uit de JSON file opgelost

--- Rolex/src/components/ProductCard.vue
+++ Rolex/src/components/ProductCard.vue
@@ -2,7 +2,7 @@
     <div class="product">
         <div class="product-content">
             <div class="product-img">
-                <img src="product.image" alt="Foto kan niet worden geladen">
+                <img :src="product.image" alt="Foto kan niet worden geladen">
             </div>
             <div class="product-btns">
                 <button type="button" class="btn-more"><span><i class="fas fa-plus"></i></span> Bekijk model
@@ -13,18 +13,21 @@
         </div>
         <div class="product-info">
             <div class="product-info-top">
-                <h2 class="product-name">DATEJUST 41</h2>
+                <h2 class="product-name">{{ product.title }}</h2>
             </div>
-            <a class="product-description">Oyster, 41mm, Oystersteel</a>
-            <p class="product-price">€ 7 500.00</p>
+            <!-- <a class="product-description">Oyster, 41mm, Oystersteel</a> -->
+            <a class="product-description">{{ product.details.braceletMaterial }}, {{ product.details.case }}, {{ product.details.caseMaterial }}</a>
+            <p class="product-price">{{ product.price }}</p>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    inject: ['producten'],
-}
+        props: {
+            product: Object
+        }
+    }
 </script>
 
 <style scoped></style>
--- Rolex/src/views/Productpage.vue
+++ Rolex/src/views/Productpage.vue
@@ -1,7 +1,12 @@
 <template>
-    <div>
-        <h1>Producten</h1>
-        <product-card v-for="product in producten" :key="product.id" :product="product" />
+    <div class="products">
+        <div class="container">
+            <h1 class="lg-title">Rolex watches</h1>
+            <p class="text-light">Bekijk de prestigieuze en uiterst nauwkeurige uurwerken uit de Rolex-collectie. Rolex
+                biedt een ruim assortiment van Oyster Perpetual- en Cellini-horloges voor aan elke pols. Ontdek de
+                uitgebreide selectie Rolex-horloges voor een perfecte combinatie van stijl en functionaliteit.</p>
+            <product-card v-for="product in producten" :key="product.id" :product="product" />
+        </div>
     </div>
 </template>
 
