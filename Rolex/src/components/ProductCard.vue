eb91c40ef9e5040ccf4eecb109b63fb180bc375f
RubenBervoets <ruben.bervoets@student.pxl.be>
2023-12-20 17:54:14 +0100

    Gewerkt aan de paginatie op de productpagina

--- Rolex/src/components/ProductCard.vue
+++ Rolex/src/components/ProductCard.vue
@@ -1,33 +1,36 @@
 <template>
-    <div class="product">
-        <div class="product-content">
-            <div class="product-img">
-                <img :src="product.image" alt="Foto kan niet worden geladen">
+    <div class="product-items">
+        <div class="product">
+            <div class="product-content">
+                <div class="product-img">
+                    <img :src="product.image" alt="Foto kan niet worden geladen">
+                </div>
+                <div class="product-btns">
+                    <button type="button" class="btn-more"><span><i class="fas fa-plus"></i></span> Bekijk model
+                    </button>
+                    <button type="button" class="btn-cart"><span><i class="fas fa-shopping-cart"></i></span> In winkelwagen
+                    </button>
+                </div>
             </div>
-            <div class="product-btns">
-                <button type="button" class="btn-more"><span><i class="fas fa-plus"></i></span> Bekijk model
-                </button>
-                <button type="button" class="btn-cart"><span><i class="fas fa-shopping-cart"></i></span> In winkelwagen
-                </button>
+            <div class="product-info">
+                <div class="product-info-top">
+                    <h2 class="product-name">{{ product.title }}</h2>
+                </div>
+                <!-- <a class="product-description">Oyster, 41mm, Oystersteel</a> -->
+                <a class="product-description">{{ product.details.braceletMaterial }}, {{ product.details.case }}, {{
+                    product.details.caseMaterial }}</a>
+                <p class="product-price">{{ product.price }}</p>
             </div>
-        </div>
-        <div class="product-info">
-            <div class="product-info-top">
-                <h2 class="product-name">{{ product.title }}</h2>
-            </div>
-            <!-- <a class="product-description">Oyster, 41mm, Oystersteel</a> -->
-            <a class="product-description">{{ product.details.braceletMaterial }}, {{ product.details.case }}, {{ product.details.caseMaterial }}</a>
-            <p class="product-price">{{ product.price }}</p>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-        props: {
-            product: Object
-        }
+    props: {
+        product: Object
     }
+}
 </script>
 
 <style scoped></style>
--- Rolex/src/views/Productpage.vue
+++ Rolex/src/views/Productpage.vue
@@ -5,7 +5,13 @@
             <p class="text-light">Bekijk de prestigieuze en uiterst nauwkeurige uurwerken uit de Rolex-collectie. Rolex
                 biedt een ruim assortiment van Oyster Perpetual- en Cellini-horloges voor aan elke pols. Ontdek de
                 uitgebreide selectie Rolex-horloges voor een perfecte combinatie van stijl en functionaliteit.</p>
-            <product-card v-for="product in producten" :key="product.id" :product="product" />
+            <!-- <product-card v-for="product in producten" :key="product.id" :product="product" /> -->
+            <product-card v-for="(product, index) in paginatedProducts" :key="product.id" :product="product" />
+            <div class="pagination">
+                <button @click="prevPage" :disabled="currentPage === 1">Vorige</button>
+                <span>{{ currentPage }}</span>
+                <button @click="nextPage" :disabled="currentPage === totalPages">Volgende</button>
+            </div>
         </div>
     </div>
 </template>
@@ -13,10 +19,48 @@
 <script>
 import ProductCard from "../components/ProductCard.vue";
 
+// export default {
+//     components: {
+//         ProductCard,
+//     },
+//     inject: ['producten'],
+// }
+
 export default {
     components: {
         ProductCard,
     },
     inject: ['producten'],
-}
+    data() {
+        return {
+            productsPerPage: 8,
+            currentPage: 1,
+        };
+    },
+    computed: {
+        totalProducts() {
+            return this.producten.length;
+        },
+        totalPages() {
+            return Math.ceil(this.totalProducts / this.productsPerPage);
+        },
+        paginatedProducts() {
+            const start = (this.currentPage - 1) * this.productsPerPage;
+            const end = start + this.productsPerPage;
+            return this.producten.slice(start, end);
+        },
+    },
+    methods: {
+        nextPage() {
+            if (this.currentPage < this.totalPages) {
+                this.currentPage++;
+            }
+        },
+        prevPage() {
+            if (this.currentPage > 1) {
+                this.currentPage--;
+            }
+        },
+    },
+};
 </script>
