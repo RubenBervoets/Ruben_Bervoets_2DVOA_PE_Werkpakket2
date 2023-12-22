9acbd1859608df9006af8f0c64d686ab3bfb532f
RubenBervoets <ruben.bervoets@student.pxl.be>
2023-12-22 21:43:50 +0100

    Login (soort van) werkende gemaakt

--- Rolex/src/views/Login.vue
+++ Rolex/src/views/Login.vue
@@ -1,37 +1,55 @@
 <template>
-    <div class="login">
+    <section class="login">
         <div class="center">
-            <h1>{{ titleForm }}</h1>
-            <form method="post">
-                <div class="txt_field">
-                    <input type="text" required>
-                    <span></span>
-                    <label>{{ label1 }}</label>
-                </div>
-                <div class="txt_field">
-                    <input type="password" required>
-                    <span></span>
-                    <label>{{ label2 }}</label>
-                </div>
-                <div class="pass">{{ forgotPass }}</div>
-                <input type="submit" value="Login">
-                <div class="signup_link">
-                    Nog geen member? <a href="#">Signup</a>
-                </div>
-            </form>
+            <div class="form-container" v-if="!loggedIn">
+                <h1>{{ titleForm }}</h1>
+                <form class="loginForm" v-on:submit.prevent="submitForm()" action="">
+                    <div class="txt_field">
+                        <label for="email">{{ label1 }}</label>
+                        <input type="email" name="email" id="email">
+                    </div>
+                    <div class="txt_field">
+                        <label for="password">{{ label2 }}</label>
+                        <input type="password" name="password" id="password">
+                    </div>
+                    <div class="pass">{{ forgotPass }}</div>
+                    <div class="form-action">
+                        <button class="loginbutton" type="submit">{{ accountButtonText }}</button>
+                    </div>
+                </form>
+            </div>
+            <div class="form-container" v-else>
+                <h1>{{ title2 }}</h1>
+                <p class="message">{{ message }}</p>
+                <button v-on:click="logout()" type="button" class="loginbutton">{{ accountButtonText }}</button>
+            </div>
         </div>
-    </div>
+    </section>
 </template>
 
-<script>
+<script >
 export default {
     data() {
         return {
             titleForm: "Login",
             label1: "E-mailadres",
             label2: "Wachtwoord",
-            forgotPass: "Wachtwoord vergeten?"
+            forgotPass: "Wachtwoord vergeten?",
+            title2: "Welkom!",
+            message: "Je bent succesvol ingelogd!",
+            accountButtonText: 'Login',
+            loggedIn: false
         }
+    },
+    methods: {
+        submitForm() {
+            this.accountButtonText = "Logout";
+            this.loggedIn = true;
+        },
+        logout() {
+            this.accountButtonText = "Login";
+            this.loggedIn = false;
+        },
     }
 }
 </script>
